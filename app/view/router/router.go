b5c0a8c1493f0458a8d9ce5bbd3276b58e435532
a <a>
2020-08-10 11:53:51 +0800

    设置member模块

--- app/view/router/router.go
+++ app/view/router/router.go
@@ -4,7 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"v2ex/app/view/controller"
 	"v2ex/app/view/controller/m_config"
-	member2 "v2ex/app/view/controller/m_member"
+	member_manage "v2ex/app/view/controller/m_member"
+	"v2ex/app/view/controller/member"
 	"v2ex/app/view/controller/view_data"
 )
 
@@ -18,16 +19,16 @@ func RegisterRoute(r *gin.Engine) {
 	r.GET("/login", controller.Login)
 
 	//会员
-	member := r.Group("/_/member")
+	member_center := r.Group("/_/member")
 
 	//会员首页
-	member.GET("/z/", member2.Index)
-	member.GET("/z/:_type/:_cz", member2.Index)
-	member.GET("/z/:_type", member2.Index)
+	member_center.GET("/z/", member_manage.Index)
+	member_center.GET("/z/:_type/:_cz", member_manage.Index)
+	member_center.GET("/z/:_type", member_manage.Index)
 
 	//文章发布
-	member.GET("/send_article", member2.SendArticle)
-	member.GET("/send_question", member2.SendQuestion)
+	member_center.GET("/send_article", member_manage.SendArticle)
+	member_center.GET("/send_question", member_manage.SendQuestion)
 
 	//超级管理员权限页面
 	r_config := r.Group("/_/config")
@@ -41,5 +42,10 @@ func RegisterRoute(r *gin.Engine) {
 	r.GET("/q/:did", view_data.Question)
 	r.GET("/q/:did/edit_answer", view_data.QuestionEditAnswer)
 	r.GET("/q/:did/answer/:rid", view_data.Question)
+
+	//会员页面
+	v_member := r.Group("/member")
+	v_member.GET("/")
+	v_member.GET("/:mid", member.Index)
 
 }
