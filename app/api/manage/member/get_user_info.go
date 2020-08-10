d3eeb8e451684a44d3bc2d217ea04af84fd51023
a <a>
2020-08-10 22:06:43 +0800

    测试

--- app/api/manage/member/get_user_info.go
+++ app/api/manage/member/get_user_info.go
@@ -10,8 +10,10 @@ import (
 func get_user_info(c *gin.Context) {
 	user_info := api.GetNowUserInfo(c)
 	result := gin.H{
-		"user_name": user_info.UserName,
-		"avatar":    common.Avatar(user_info.Avatar),
+		"user_name":    user_info.UserName,
+		"avatar":       common.Avatar(user_info.Avatar),
+		"des":          user_info.More.Des,
+		"des_detailed": user_info.More.DesDetailed,
 	}
 	result_json := c_code.V1GinSuccess(result)
 	c.JSON(200, result_json)
