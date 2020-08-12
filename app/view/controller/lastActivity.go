2f52f08c2171835e8a6a338e335a9c1dfc7b385b
a <a>
2020-08-12 10:41:56 +0800

    动态页完成

--- app/view/controller/lastActivity.go
+++ app/view/controller/lastActivity.go
@@ -1,11 +1,16 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/123456/c_code"
+	"github.com/123456/c_code/mc"
 	"github.com/gin-gonic/gin"
+	"github.com/globalsign/mgo/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
 	"v2ex/app/api/member"
 	"v2ex/app/view"
+	"v2ex/model"
 )
 
 func LastActivity(c *gin.Context) {
@@ -34,6 +39,45 @@ func LastActivity(c *gin.Context) {
 		c.Redirect(301, "/last_activity")
 	}
 	//加载热门问题
+	t_30 := time.Now().AddDate(0, 0, -30)
+	fmt.Println(t_30.String())
+	index_t := model.DataIndex{}
+	_article := []model.DataIndex{}
+	article := []gin.H{}
+	mc.Table(index_t.Table()).Where(bson.M{"ct": bson.M{"$gt": t_30.Unix()}, "d_type": model.DTYPEArticle}).Order(bson.M{"rc": -1}).Limit(10).Find(&_article)
+	//如果不足10 条则以最新的数据填充
+	if len(_article) != 10 {
+		_a := []model.DataIndex{}
+		mc.Table(index_t.Table()).Where(bson.M{"d_type": model.DTYPEArticle}).Order(bson.M{"_id": -1}).Limit(int64(10 - len(_article))).Find(&_a)
+		_article = append(_article, _a...)
+	}
+
+	for _, v := range _article {
+		article = append(article, gin.H{
+			"t": v.T,
+			"u": model.UrlArticle(v),
+		})
+	}
+	_ht["article"] = article
+	//得到热门问题
+	_question := []model.DataIndex{}
+	question := []gin.H{}
+	mc.Table(index_t.Table()).Where(bson.M{"ct": bson.M{"$gt": t_30.Unix()}, "d_type": model.DTYPEQuestion}).Order(bson.M{"rc": -1}).Limit(10).Find(&_question)
+
+	if len(_question) != 10 {
+		_a := []model.DataIndex{}
+		mc.Table(index_t.Table()).Where(bson.M{"d_type": model.DTYPEQuestion}).Order(bson.M{"_id": -1}).Limit(int64(10 - len(_article))).Find(&_a)
+		_question = append(_question, _a...)
+	}
+
+	for _, v := range _question {
+		question = append(question, gin.H{
+			"t":  v.T,
+			"u":  model.UrlArticle(v),
+			"rc": v.RC,
+		})
+	}
+	_ht["question"] = question
 
 	view.Render(c, "_last_activity", _ht)
 }
